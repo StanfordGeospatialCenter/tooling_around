f71f57bd1d3d7966ef44186a6d6f824161ad1632
mapninja <stacey.maples@gmail.com>
2024-09-24 14:44:18 -0700

    Download and plot WFS layer as GeoJSON file in R

--- snippet_templates/r_vector_open.r
+++ snippet_templates/r_vector_open.r
@@ -1,18 +1,28 @@
+# This code downloads and plots a WFS layer as a GeoJSON file using R
 # Load the necessary libraries
 library(sf)
 library(ggplot2)
 
 # Define the WFS service URL and typename
-geoserver_url <- "https://geowebservices.stanford.edu/geoserver/druid/ows?"
+geoserver_url <- "https://geowebservices.stanford.edu/geoserver/wfs?"
 type_name <- "druid:vx572wx7854"
+
 # Define the WFS request URL
 wfs_url <- paste0(
     geoserver_url,
     "service=WFS&version=1.0.0&request=GetFeature&typeName=",
     type_name, "&outputFormat=application/json"
 )
-# Download the WFS layer
-wfs_layer <- st_read(wfs_url)
+
+# Define the local file name based on type_name
+local_geojson <- paste0(type_name, ".geojson")
+
+# Download the WFS layer as a GeoJSON file
+download.file(wfs_url, destfile = local_geojson, mode = "wb")
+
+# Read the GeoJSON file from the local disk
+wfs_layer <- st_read(local_geojson)
+
 # Plot the WFS layer
 ggplot() +
     geom_sf(data = wfs_layer) +
